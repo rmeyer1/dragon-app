26295e6e649c097c34ee2912abe5f9a99aabe032
rmeyer1 <robmeyer03@gmail.com>
2024-12-27 16:49:17 -0500

    fix linting error

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 import emailjs from '@emailjs/browser';
 import './ContactPage.css';
 
